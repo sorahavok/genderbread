2418c452d2fe57fb1750c453efccd54da4aa06b5
sorahavok <sora.havok@gmail.com>
2015-08-15 16:55:46 -0700

    Simple input and display for a person

--- genderbread.js
+++ genderbread.js
@@ -1,17 +1,35 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Persons = new Mongo.Collection("persons");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if (Meteor.isClient) {
+
+  Template.body.helpers({
+    persons : function() {
+      return Persons.find({});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.createPerson.events({
+    "submit .new-person": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var name = event.target.name.value;
+      var womanId = event.target.womanId.value;
+      var manId = event.target.manId.value;
+
+      // Insert a task into the collection
+      Persons.insert({
+        name: name,
+        womanId: womanId,
+        manId: manId,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.name.value = "";
+      event.target.womanId.value = "";
+      event.target.manId.value = "";
     }
   });
 }
