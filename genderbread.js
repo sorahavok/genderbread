c1428cdbdb3ff0d21f12032d000802569852bd71
sorahavok <sora.havok@gmail.com>
2015-08-15 18:53:26 -0700

    Started using the accounts and added Close Users

--- genderbread.js
+++ genderbread.js
@@ -5,8 +5,24 @@ if (Meteor.isClient) {
   Template.body.helpers({
     persons : function() {
       return Persons.find({});
+    },
+    closePersons : function() {
+      user = Session.get("curUser") || Persons.findOne({owner: Meteor.userId()})
+      return Persons.find({
+        $and:[
+          {name: {$not:user.name}},
+          {$or:[{womanId:user.womanId}, {manId:user.manId}]}
+          ]
+        }
+      );
     }
   });
+
+  Template.userInfo.helpers({
+    curUser : function() {
+      return Session.get("curUser")
+    }
+  })
 
   Template.createPerson.events({
     "submit .new-person": function (event) {
@@ -15,14 +31,16 @@ if (Meteor.isClient) {
 
       // Get value from form element
       var name = event.target.name.value;
-      var womanId = event.target.womanId.value;
-      var manId = event.target.manId.value;
+      var womanId = parseInt(event.target.womanId.value);
+      var manId = parseInt(event.target.manId.value);
 
       // Insert a task into the collection
       Persons.insert({
         name: name,
         womanId: womanId,
         manId: manId,
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username,  // username of logged in user
         createdAt: new Date() // current time
       });
 
@@ -31,6 +49,14 @@ if (Meteor.isClient) {
       event.target.womanId.value = "";
       event.target.manId.value = "";
     }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+  Accounts.onLogin(function (){
+    Session.set('curUser', Persons.findOne({owner: Meteor.userId()})); 
   });
 }
 
